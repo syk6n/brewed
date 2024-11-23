efd0b3fbf3273297b7a10c3a12ceb34dc58f8b4e
syk6n <sayankunduatwork@gmail.com>
2024-11-24 02:44:22 +0530

    cryp show

--- src/cryptoverse.tsx
+++ src/cryptoverse.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import CryptoVerse from './pages/CryptoVerse';
+import Cryptoverse from './pages/Cryptoverse';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <CryptoVerse />
+    <Cryptoverse />
   </StrictMode>
 );
